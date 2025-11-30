734c7affb56fd21da12fd4c8988b75f20e5bf4f0
ENDESGA <38831818+ENDESGA@users.noreply.github.com>
2025-11-30 23:52:46 +1300

    0.3: Add GDB-Python Support and windres.exe

--- build.sh
+++ build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
-###----------------------------------------------------------------------------
-# TinyGW Build Script - @ENDESGA - 2025 - Made in NZ - CC0 - foss forever
-###----------------------------------------------------------------------------
+################################################################
+##
+##  TinyGW Build Script
+##
+##  author(s):
+##  ENDESGA - https://x.com/ENDESGA | https://bsky.app/profile/endesga.bsky.social
+##
+##  https://github.com/ENDESGA/TinyGW
+##  2025 - CC0 - FOSS forever
+##
 
 set -euo pipefail
 
@@ -11,62 +18,83 @@ echo "> installing packages..."
 pacman -S --noconfirm --needed mingw-w64-x86_64-gcc mingw-w64-x86_64-binutils mingw-w64-x86_64-gdb
 echo "> packages installed."
 
-M="/mingw64"
-N="x86_64-w64-mingw32"
-V=$(gcc -dumpversion)
+MGW="/mingw64"
+TGW="tinygw"
+XWMGW="x86_64-w64-mingw32"
+GCCV=$(gcc -dumpversion)
 
-G="$M/lib/gcc/$N/$V"
-I="tinygw/lib/gcc/$N/$V/include"
+MBIN="$MGW/bin"
+TBIN="$TGW/bin"
+MLGCC="$MGW/lib/gcc/$XWMGW/$GCCV"
+TLGCC="$TGW/lib/gcc/$XWMGW/$GCCV"
+TMGW="$TGW/$XWMGW"
+TGINC="$TLGCC/include"
+MPY="$MGW/lib/python3.12"
+TPY="$TGW/lib/python3.12"
 
 echo -n "> creating directory structure..."
-rm -rf tinygw
-mkdir -p tinygw/bin tinygw/lib/gcc/$N/$V $I tinygw/$N/lib
+rm -rf "$TGW"
+mkdir -p "$TBIN" "$TLGCC" "$TGINC" "$TMGW/lib" "$TGW/share/gdb/python" "$TPY"
 echo " done"
 
 echo -n "> copying binaries..."
-for bin in {gcc,gdb}.exe;
-	do cp "$M/bin/$bin" tinygw/bin/; done
-for bin in {as,ld}.exe;
-	do cp "$M/bin/$bin" tinygw/lib/gcc/$N/$V/; done
-for bin in {cc1,collect2,lto-wrapper,lto1}.exe;
-	do cp "$G/$bin" tinygw/lib/gcc/$N/$V/; done
-cp $G/liblto_plugin*.dll tinygw/lib/gcc/$N/$V/
+for bin in {gcc,gdb,windres}.exe; do
+	cp "$MBIN/$bin" "$TBIN/"
+done
+for bin in {as,ld}.exe; do
+	cp "$MBIN/$bin" "$TLGCC/"
+done
+for bin in {cc1,collect2,lto-wrapper,lto1}.exe; do
+	cp "$MLGCC/$bin" "$TLGCC/"
+done
+cp $MLGCC/liblto_plugin*.dll "$TLGCC/"
 echo " done"
 
 echo -n "> copying dynamic-link libraries..."
-ldd tinygw/bin/{gcc,gdb}.exe | awk '/mingw64/ && $3 {print $3}' | sort -u | while read dll;
-	do cp "$dll" tinygw/bin/; done
-ldd tinygw/lib/gcc/$N/$V/{as,ld,cc1,collect2,lto-wrapper,lto1}.exe | awk '/mingw64/ && $3 {print $3}' | sort -u | while read dll;
-	do cp "$dll" tinygw/lib/gcc/$N/$V/; done
+ldd "$TBIN"/{gcc,gdb}.exe | awk '/mingw64/ && $3 {print $3}' | sort -u | while read dll; do
+	cp "$dll" "$TBIN/"
+done
+ldd "$TLGCC"/{as,ld,cc1,collect2,lto-wrapper,lto1}.exe | awk '/mingw64/ && $3 {print $3}' | sort -u | while read dll; do
+	cp "$dll" "$TLGCC/"
+done
 echo " done"
 
 echo -n "> copying headers..."
-cp $G/include/*.h $I/
-mkdir -p tinygw/$N/include
-cp -r $M/include/* tinygw/$N/include/
-for dir in c++ ddk gdiplus GL isl KHR libiberty lzma ncurses ncursesw openssl python* readline tcl* tk* tre wrl X11 gdb qt gtk* glib* pango* cairo* atk*;
-	do 
-		rm -rf $I/$dir
-		rm -rf tinygw/$N/include/$dir
-	done
+cp $MLGCC/include/*.h "$TGINC/"
+mkdir -p "$TMGW/include"
+for ext in h idl inl; do
+	cp "$MGW/include"/*.$ext "$TMGW/include/" 2>/dev/null || true
+done
+for dir in psdk_inc python3.12 sdks sec_api sys tcl8.6 tk8.6; do
+	cp -r "$MGW/include/$dir" "$TMGW/include/" 2>/dev/null || true
+done
 echo " done"
 
 echo -n "> copying objects and libraries..."
-for obj in crt*.o libgcc*.a;
-	do cp $G/$obj tinygw/lib/gcc/$N/$V/; done
-for obj in {crt2,crtbegin,crtend,default-manifest,dllcrt2}.o;
-	do cp "$M/lib/$obj" tinygw/$N/lib/; done
-for lib in {libmingw32,libmingwex,libmsvcrt,libkernel32,libpthread,libuser32,libgdi32,libadvapi32,libshell32,libm,libmoldname,libgcc_s}.a;
-	do cp "$M/lib/$lib" tinygw/$N/lib/; done
+for obj in crt*.o libgcc*.a; do
+	cp $MLGCC/$obj "$TLGCC/"
+done
+for obj in {crt2,crtbegin,crtend,default-manifest,dllcrt2}.o; do
+	cp "$MGW/lib/$obj" "$TMGW/lib/"
+done
+for lib in {libmingw32,libmingwex,libmsvcrt,libkernel32,libpthread,libuser32,libgdi32,libadvapi32,libshell32,libm,libmoldname,libgcc_s}.a; do
+	cp "$MGW/lib/$lib" "$TMGW/lib/"
+done
+echo " done"
+
+echo -n "> copying gdb python support..."
+cp -r "$MGW/share/gdb/python/gdb" "$TGW/share/gdb/python/"
+cp "$MPY"/*.py "$TPY/"
+for dir in collections encodings importlib json lib-dynload re; do
+	cp -r "$MPY/$dir" "$TPY/"
+done
 echo " done"
 
 echo ""
 echo "> build complete:"
-echo "	bin dlls: $(ls tinygw/bin/*.dll 2>/dev/null | wc -l)"
-echo "	lib dlls: $(ls tinygw/lib/gcc/$N/$V/*.dll 2>/dev/null | wc -l)"
-echo "	gcc headers: $(find $I -type f -name '*.h' 2>/dev/null | wc -l)"
-echo "	mingw headers: $(find tinygw/$N/include/ -type f -name '*.h' 2>/dev/null | wc -l)"
-echo "	libraries: $(ls tinygw/$N/lib/*.a 2>/dev/null | wc -l)"
-
-echo "	size: $(du -sh tinygw | cut -f1)"
-
+echo "	bin dlls: $(ls "$TBIN"/*.dll 2>/dev/null | wc -l)"
+echo "	lib dlls: $(ls "$TLGCC"/*.dll 2>/dev/null | wc -l)"
+echo "	gcc headers: $(find "$TGINC" -type f -name '*.h' 2>/dev/null | wc -l)"
+echo "	mingw headers: $(find "$TMGW/include" -type f -name '*.h' 2>/dev/null | wc -l)"
+echo "	libraries: $(ls "$TMGW/lib"/*.a 2>/dev/null | wc -l)"
+echo "	size: $(du -sh "$TGW" | cut -f1)"
